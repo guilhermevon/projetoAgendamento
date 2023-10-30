288b5186176e6155b3351bca5460c0a0add327d6
matthew2414 <99284372+matthew2414@users.noreply.github.com>
2023-10-30 11:31:20 -0300

    project base

---
+++ Calendario/src/components/cadastro.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import '../styles/cadastro.css'
+
+function Cadastro() {
+
+  return (
+    <>
+      <div>
+        <h1>Cadastro</h1>
+      </div>
+    </>
+  )
+}
+
+export default Cadastro
---
+++ Calendario/src/components/calendario.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './styles/cadastro.css'
+
+function Calendario() {
+
+  return (
+    <>
+      <div>
+        <h1>Calendario</h1>
+      </div>
+    </>
+  )
+}
+
+export default Calendario
