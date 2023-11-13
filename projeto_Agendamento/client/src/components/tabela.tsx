3958f60ea8caf7ea5e155b2f4243b8de1a130d40
matthew2414 <99284372+matthew2414@users.noreply.github.com>
2023-11-13 11:15:23 -0300

    Add files via upload

---
+++ projeto_Agendamento/client/src/components/tabela.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './styles/cadastro.css'
+
+function Tabela() {
+
+  return (
+    <>
+      <div>
+        <h1>Tabela</h1>
+      </div>
+    </>
+  )
+}
+
+export default Tabela
